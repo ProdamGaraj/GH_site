76c01c31afbeb6a73fe7ed30b763fbb621213951
d_tolkunov <d.tolkunov@gh.uz>
2026-08-25 11:47:53 +0500

    Fix header on estate page

--- visual-cms/frontend/src/pages/EstateEditorPage.tsx
+++ visual-cms/frontend/src/pages/EstateEditorPage.tsx
@@ -1,3 +1,11 @@
+import { Header } from '@/shared/components/Header'
 import { EstateEditor } from '@/features/estate'
 
-export const EstateEditorPage = () => <EstateEditor />
+export const EstateEditorPage = () => (
+  <div className="h-screen flex flex-col">
+    <Header />
+    <div className="flex-1 overflow-y-auto">
+      <EstateEditor />
+    </div>
+  </div>
+)
--- visual-cms/frontend/src/pages/EstatePage.tsx
+++ visual-cms/frontend/src/pages/EstatePage.tsx
@@ -1,3 +1,11 @@
+import { Header } from '@/shared/components/Header'
 import { EstateList } from '@/features/estate'
 
-export const EstatePage = () => <EstateList />
+export const EstatePage = () => (
+  <div className="h-screen flex flex-col">
+    <Header />
+    <div className="flex-1 overflow-y-auto">
+      <EstateList />
+    </div>
+  </div>
+)
